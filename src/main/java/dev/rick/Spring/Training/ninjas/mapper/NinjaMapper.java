fb02a5c1d94c4ea2d7b1c3bb381f62ddafe31c62
Henrique-Camargo <henriquecnobrega0711@gmail.com>
2025-07-23 22:38:53 -0300

    Chore: Inclue a versao do DTO pela entidade e mapea manualmente no ninjaMapper os caminhos entidadexDTO

--- src/main/java/dev/rick/Spring/Training/ninjas/mapper/NinjaMapper.java
+++ src/main/java/dev/rick/Spring/Training/ninjas/mapper/NinjaMapper.java
@@ -1,4 +1,35 @@
 package dev.rick.Spring.Training.ninjas.mapper;
 
+import dev.rick.Spring.Training.ninjas.dto.NinjaDTO;
+import dev.rick.Spring.Training.ninjas.model.NinjaModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgURL(ninjaDTO.getImgURL());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setImgURL(ninjaModel.getImgURL());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
+
 }
